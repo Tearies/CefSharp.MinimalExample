71c45cba478657eb764ea82adb97e83399f0df20
Tearies <xiangmaojun@techstar.com.cn>
2020-09-22 13:34:45 +0800

    修复编译的问题

--- CefSharp.MinimalExample.Common/browser/CefSetting.cs
+++ CefSharp.MinimalExample.Common/browser/CefSetting.cs
@@ -6,7 +6,7 @@ using CefSharp.MinimalExample.Common.args;
 
 namespace CefSharp.MinimalExample.Common.browser
 {
-    public class CefSetting : AbstractCefSettings
+    public class CefSetting : CefSettingsBase
     {
 
         public Target CurrentRuntimeTarget
